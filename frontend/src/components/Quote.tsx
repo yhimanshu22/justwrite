26991c5285f44cffade4e814b441f52ab3daf55c
HIMANSHU YADAV <yhimanshu22@iitk.ac.in>
2024-07-11 20:52:53 +0530

    added quote

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,19 +1,23 @@
 
 export const Quote = () => {
-    return <div className="bg-slate-200 h-screen flex justify-center flex-col">
-        <div className="flex justify-center">
-            <div className="max-w-lg">
-                <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
-                </div>
-                <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
-                </div>
-                <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+    const quoteText = "Join us to express your passions, connect with fellow bloggers and readers through vibrant discussions, and effortlessly grow your audience on our platform.";
+    const author = "Himanshu Yadav"; // Replace with the name of the famous writer
+
+    return (
+        <div className="bg-slate-200 h-screen flex justify-center flex-col">
+            <div className="flex justify-center">
+                <div className="max-w-lg">
+                    <div className="text-3xl font-bold">
+                        {quoteText}
+                    </div>
+                    <div className="max-w-md text-xl font-semibold text-left mt-4">
+                        — {author}
+                    </div>
+                    <div className="max-w-md text-mi font-light text-slate-500">
+                        {'JustWrite Founder'}
+                    </div>
                 </div>
             </div>
         </div>
-        
-    </div>
+    );
 }
